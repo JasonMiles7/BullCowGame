d43550b70ec2f5b96a342a3d09e211900a1ed86a
JasonMiles7 <minecraftguy@talktalk.net>
2017-02-27 20:32:39 +0000

    27/02/17

--- FBullCowGame.cpp
+++ FBullCowGame.cpp
@@ -2,26 +2,10 @@
 
 using int32 = int;
 
-FBullCowGame::FBullCowGame() {Reset(); }
+FBullCowGame::FBullCowGame() { Reset(); }
 
-int32 FBullCowGame::GetMaxTries() const { return MyMaxTries;}
+int32 FBullCowGame::GetMaxTries() const { return MyMaxTries; }
 int32 FBullCowGame::GetCurrentTry() const { return MyCurrentTry; }
-
-
-FBullCowGame::FBullCowGame()
-{
-	Reset();
-}
-
-int FBullCowGame::GetMaxTries() const
-{
-	return MyMaxTries;
-}
-
-int FBullCowGame::GetCurrentTry() const
-{
-	return MyCurrentTry;
-}
 
 bool FBullCowGame::IsGameWon() const
 {
